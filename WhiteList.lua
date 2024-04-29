807f79c4337fc9d2adb67ed819d1a52306372c1a
timYTHuber <112040081+timYTHuber@users.noreply.github.com>
2024-04-29 13:02:34 +0700

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -1,7 +1,7 @@
 local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/timYTHuber/WhiteList/main/DataBase", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/timYTHuber/AllInOne/main/Hub.lua", true))() -- Ваш скрипт
+  https://raw.githubusercontent.com/timYTHuber/AllInOne/main/Hub.lua -- Ваш скрипт
 else
   game:service('Players').LocalPlayer:Kick('Bruh')
 end
